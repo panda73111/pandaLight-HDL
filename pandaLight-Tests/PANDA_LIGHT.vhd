2eb6e38c83ab65517b9b9d5871aa2064efae7655
Sebastian Hüther <sebastian.huether@gmx.de>
2014-07-28 23:20:38 +0200

    Created PANDA_LIGHT top module

---
+++ pandaLight-Tests/PANDA_LIGHT.vhd
@@ -0,0 +1,30 @@
+----------------------------------------------------------------------------------
+-- Engineer: Sebastian Huether
+-- 
+-- Create Date:    21:49:35 07/28/2014 
+-- Module Name:    PANDA_LIGHT - rtl 
+-- Project Name:   PANDA_LIGHT
+-- Tool versions:  Xilinx ISE 14.7
+-- Description: 
+--
+-- Revision: 0
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+library UNISIM;
+use UNISIM.VComponents.all;
+
+entity PANDA_LIGHT is
+end PANDA_LIGHT;
+
+architecture rtl of PANDA_LIGHT is
+
+begin
+
+
+end rtl;
+
