a76d687b6e8dfabb3fd19874491f8ab337541b68
Sebastian Hüther <sebastian.huether@gmx.de>
2014-07-28 17:34:17 +0200

    Corrected a comment

--- TMDS_CHANNEL_ENCODER/TMDS_CHANNEL_ENCODER.vhd
+++ TMDS_CHANNEL_ENCODER/TMDS_CHANNEL_ENCODER.vhd
@@ -195,10 +195,10 @@ begin
     enc_din_proc : process(din_ones_cnt, DATA_IN(0), din_xor, din_xnor)
     begin
         if din_ones_cnt > 4 or (din_ones_cnt = 4 and DATA_IN(0) = '0') then
-            -- use xored data
+            -- use xnored data
             din_enc <= din_xnor;
         else
-            -- use xnored data
+            -- use xored data
             din_enc <= din_xor;
         end if;
     end process;
