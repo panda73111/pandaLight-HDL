32f504c762e64a2b07699f33094e71169cfce14c
Sebastian Hüther <sebastian.huether@gmx.de>
2014-08-05 17:36:32 +0200

    Write v2 of LED_CORRECTION, this time using state machines

--- LED_CORRECTION/LED_CORRECTION_tb.vhd
+++ LED_CORRECTION/LED_CORRECTION_tb.vhd
@@ -25,34 +25,38 @@ END LED_CORRECTION_tb;
 ARCHITECTURE behavior OF LED_CORRECTION_tb IS 
 
     --Inputs
-    signal clk              : std_ulogic := '0';
-    signal rst              : std_ulogic := '0';
-    signal cfg_addr         : std_ulogic_vector(1 downto 0) := (others => '0');
-    signal cfg_wr_en        : std_ulogic := '0';
-    signal cfg_data         : std_ulogic_vector(7 downto 0) := x"00";
-    signal led_vsync_in     : std_ulogic := '0';
-    signal led_rgb_in       : std_ulogic_vector(23 downto 0) := x"000000";
-    signal led_rgb_in_wr_en : std_ulogic := '0';
+    signal clk  : std_ulogic := '0';
+    signal rst  : std_ulogic := '0';
+    
+    signal cfg_addr     : std_ulogic_vector(1 downto 0) := (others => '0');
+    signal cfg_wr_en    : std_ulogic := '0';
+    signal cfg_data     : std_ulogic_vector(7 downto 0) := x"00";
+    
+    signal led_in_vsync : std_ulogic := '0';
+    signal led_in_num   : std_ulogic_vector(7 downto 0) := x"00";
+    signal led_in_rgb   : std_ulogic_vector(23 downto 0) := x"000000";
+    signal led_in_wr_en : std_ulogic := '0';
 
     --outputs
-    signal led_vsync_out        : std_ulogic := '0';
-    signal led_rgb_out          : std_ulogic_vector(23 downto 0) := x"000000";
-    signal led_rgb_out_valid    : std_ulogic := '0';
+    signal led_out_vsync    : std_ulogic := '0';
+    signal led_out_rgb      : std_ulogic_vector(23 downto 0) := x"000000";
+    signal led_out_valid    : std_ulogic := '0';
 
     -- Clock period definitions
     constant clk_period : time := 10 ns;
     
     type mode_type is (RGB, RBG, GRB, GBR, BRG, BGR);
     signal cur_mode : mode_type := RGB;
-    signal cur_start_led_num    : natural := 0;
-    signal cur_frame_delay      : natural := 0;
+    signal cur_start_led_num    : natural range 0 to 255 := 0;
+    signal cur_frame_delay      : natural range 0 to 255 := 0;
     
 BEGIN
     
     LED_CORRECTION_inst : entity work.LED_CORRECTION
         generic map (
             MAX_LED_COUNT   => 100,
-            MAX_BUFFER_SIZE => 10240
+             -- 32 frames = ~1 second of delay at 30 fps
+            MAX_BUFFER_SIZE => 32*128
         )
         port map (
             CLK => clk,
@@ -62,13 +66,14 @@ BEGIN
             CFG_WR_EN   => cfg_wr_en,
             CFG_DATA    => cfg_data,
             
-            LED_VSYNC_IN        => led_vsync_in,
-            LED_RGB_IN          => led_rgb_in,
-            LED_RGB_IN_WR_EN    => led_rgb_in_wr_en,
+            LED_IN_VSYNC    => led_in_vsync,
+            LED_IN_NUM      => led_in_num,
+            LED_IN_RGB      => led_in_rgb,
+            LED_IN_WR_EN    => led_in_wr_en,
             
-            LED_VSYNC_OUT       => led_vsync_out,
-            LED_RGB_OUT         => led_rgb_out,
-            LED_RGB_OUT_VALID   => led_rgb_out_valid
+            LED_OUT_VSYNC   => led_out_vsync,
+            LED_OUT_RGB     => led_out_rgb,
+            LED_OUT_VALID   => led_out_valid
         );
     
     
@@ -97,37 +102,39 @@ BEGIN
         wait for CLK_period*10;
         wait until rising_edge(clk);
         
-        -- set 100 test colors
-        configure("00", stdulv(100, 8));
+        -- set 50 test colors
+        configure("00", stdulv(50, 8));
         
         for mode_i in 0 to 5 loop
             cur_mode    <= mode_type'val(mode_i);
             
             configure("11", stdulv(mode_i, 8));
             
-            for start_led_num in 0 to 99 loop
+            for start_led_num in 0 to 49 loop
                 cur_start_led_num   <= start_led_num;
                 
                 configure("01", stdulv(start_led_num, 8));
                 
-                for frame_delay in 0 to 59 loop
+                for frame_delay in 0 to 29 loop
                     cur_frame_delay <= frame_delay;
                     
                     configure("10", stdulv(frame_delay, 8));
                     
-                    for led_i in 0 to 199 loop
-                        led_vsync_in    <= '1';
+                    for frame_i in 0 to 199 loop
+                        led_in_vsync    <= '1';
                         
                         r   := x"FF";
                         g   := x"00";
                         b   := x"7F";
                         
-                        for i in 0 to 99 loop
+                        for led_i in 0 to 49 loop
                             
-                            led_rgb_in          <= r & g & b;
-                            led_rgb_in_wr_en    <= '1';
+                            led_in_num  <= stdulv(led_i, 8);
+                            
+                            led_in_rgb      <= r & g & b;
+                            led_in_wr_en    <= '1';
                             wait until rising_edge(clk);
-                            led_rgb_in_wr_en    <= '0';
+                            led_in_wr_en    <= '0';
                             wait for clk_period*10;
                             
                             r   := r-1;
@@ -136,7 +143,7 @@ BEGIN
                             
                         end loop;
                         
-                        led_vsync_in   <= '0';
+                        led_in_vsync   <= '0';
                         wait for 100*clk_period;
                         wait until rising_edge(clk);
                     end loop;
