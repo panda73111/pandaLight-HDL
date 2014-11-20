cc70f897f642d34af1608ef556f676b1725349fa
Sebastian Hüther <sebastian.huether@gmx.de>
2014-11-20 03:48:26 +0100

    Moved main testing project to subdirectory for HDMI port specific test

--- pandaLight-Tests/HDMI passthrough test/PANDA_LIGHT.vhd
+++ pandaLight-Tests/HDMI passthrough test/PANDA_LIGHT.vhd
@@ -43,33 +43,7 @@ entity PANDA_LIGHT is
         TX_SCL              : inout std_ulogic := 'Z';
         TX_CEC              : inout std_ulogic := 'Z';
         TX_DET              : in std_ulogic := '0';
-        TX_EN               : out std_ulogic := '0';
-        
-        -- USB UART
-        USB_RXD     : in std_ulogic;
-        USB_TXD     : out std_ulogic := '0';
-        USB_CTSN    : in std_ulogic;
-        USB_RTSN    : out std_ulogic := '0';
-        USB_DSRN    : in std_ulogic;
-        USB_DTRN    : out std_ulogic := '0';
-        USB_DCDN    : out std_ulogic := '0';
-        USB_RIN     : out std_ulogic := '0';
-        
-        -- Blutooth UART
-        BT_TXD  : out std_ulogic := '0';
-        BT_RXD  : in std_ulogic;
-        BT_RTS  : out std_ulogic := '0';
-        BT_CTS  : in std_ulogic;
-        
-        -- LED strip
-        LEDS_CLK    : out std_ulogic_vector(1 downto 0) := "00";
-        LEDS_DATA   : out std_ulogic_vector(1 downto 0) := "00";
-        
-        -- PMOD
-        PMOD0   : out std_ulogic_vector(3 downto 0) := "0000";
-        PMOD1   : out std_ulogic_vector(3 downto 0) := "0000";
-        PMOD2   : out std_ulogic_vector(3 downto 0) := "0000";
-        PMOD3   : out std_ulogic_vector(3 downto 0) := "0000"
+        TX_EN               : out std_ulogic := '0'
     );
 end PANDA_LIGHT;
 
@@ -93,7 +67,8 @@ architecture rtl of PANDA_LIGHT is
     --- HDMI related signals ---
     ----------------------------
     
-    signal rx_edid_ready    : std_ulogic_vector(1 downto 0) := "00";
+    signal rx_det_stable    : std_ulogic := '0';
+    signal tx_det_stable    : std_ulogic := '0';
     
     signal rx_channels_in   : std_ulogic_vector(7 downto 0) := x"00";
     signal tx_channels_out  : std_ulogic_vector(3 downto 0) := "0000";
@@ -139,72 +114,6 @@ architecture rtl of PANDA_LIGHT is
     signal rx_aux_data_valid    : std_ulogic := '0';
     
     
-    ----------------------------
-    --- LED colour extractor ---
-    ----------------------------
-    
-    -- Inputs
-    signal ledex_clk    : std_ulogic := '0';
-    signal ledex_rst    : std_ulogic := '0';
-    
-    signal ledex_cfg_addr   : std_ulogic_vector(3 downto 0) := "0000";
-    signal ledex_cfg_wr_en  : std_ulogic := '0';
-    signal ledex_cfg_data   : std_ulogic_vector(7 downto 0) := x"00";
-    
-    signal ledex_frame_vsync    : std_ulogic := '0';
-    signal ledex_frame_hsync    : std_ulogic := '0';
-    
-    signal ledex_frame_rgb  : std_ulogic_vector(23 downto 0) := x"000000";
-    
-    -- Outputs
-    signal ledex_led_vsync  : std_ulogic := '0';
-    signal ledex_led_valid  : std_ulogic := '0';  
-    signal ledex_led_num    : std_ulogic_vector(7 downto 0) := x"00";
-    signal ledex_led_rgb    : std_ulogic_vector(23 downto 0) := x"000000";
-    
-    
-    -------------------
-    --- LED control ---
-    -------------------
-    
-    -- Inputs
-    signal ledctrl_clk  : std_ulogic := '0';
-    signal ledctrl_rst  : std_ulogic := '0';
-    
-    signal ledctrl_mode : std_ulogic_vector(1 downto 0);
-    
-    signal ledctrl_led_vsync        : std_ulogic := '0';
-    signal ledctrl_led_rgb          : std_ulogic_vector(23 downto 0);
-    signal ledctrl_led_rgb_wr_en    : std_ulogic := '0';
-    
-    -- Outputs
-    signal ledctrl_leds_clk     : std_ulogic := '0';
-    signal ledctrl_leds_data    : std_ulogic := '0';
-    
-    
-    ----------------------
-    --- LED correction ---
-    ----------------------
-    
-    -- Inputs
-    signal ledcorr_clk  : std_ulogic := '0';
-    signal ledcorr_rst  : std_ulogic := '0';
-    
-    signal ledcorr_cfg_addr     : std_ulogic_vector(1 downto 0) := "00";
-    signal ledcorr_cfg_wr_en    : std_ulogic := '0';
-    signal ledcorr_cfg_data     : std_ulogic_vector(7 downto 0) := x"00";
-    
-    signal ledcorr_led_in_vsync : std_ulogic := '0';
-    signal ledcorr_led_in_num   : std_ulogic_vector(7 downto 0) := x"00";
-    signal ledcorr_led_in_wr_en : std_ulogic := '0';
-    signal ledcorr_led_in_rgb   : std_ulogic_vector(23 downto 0) := x"000000";
-    
-    -- Outputs
-    signal ledcorr_led_out_vsync    : std_ulogic := '0';
-    signal ledcorr_led_out_valid    : std_ulogic := '0';
-    signal ledcorr_led_out_rgb      : std_ulogic_vector(23 downto 0) := x"000000";
-    
-    
     -----------------------------
     --- RX to TX0 passthrough ---
     -----------------------------
@@ -232,6 +141,8 @@ begin
             DIVISOR         => 2
         )
         port map (
+            RST => '0',
+            
             CLK_IN          => CLK20,
             CLK_OUT         => g_clk,
             CLK_OUT_STOPPED => g_clk_stopped
@@ -242,37 +153,39 @@ begin
     ------ global signal management ------
     --------------------------------------
     
-    USB_TXD     <= microblaze_txd when uart_select='0' else '0';
-    USB_RTSN    <= not microblaze_gpo1(11) when uart_select='0' else '1';
-    
-    BT_TXD  <= microblaze_txd when uart_select='1' else '0';
-    BT_RTS  <= microblaze_gpo1(11) when uart_select='1' else '0';
-    
-    LEDS_CLK(0)     <= ledctrl_leds_clk;
-    LEDS_DATA(0)    <= ledctrl_leds_data;
-    
     g_rst   <= g_clk_stopped;
-    
-    uart_select <= microblaze_gpo1(12);
     
     
     ------------------------------------
     ------ HDMI signal management ------
     ------------------------------------
     
-    RX_EN   <= rx_edid_ready;
-    
-    -- drive low dominant I2C signals
-    RX_SDA(RX_SEL)  <= '0' when eddc_s_sda_out='0' else 'Z';
-    RX_SCL(RX_SEL)  <= '0' when eddc_s_scl_out='0' else 'Z';
-    TX_SDA          <= '0' when eddc_m_sda_out='0' else 'Z';
-    TX_SCL          <= '0' when eddc_m_scl_out='0' else 'Z';
-    
-    rx_edid_ready   <= stdulv(microblaze_gpo1(14 downto 13));
-    
-    TX_EN   <= rx_enc_data_valid;
+    RX_EN(RX_SEL)   <= tx_det_stable;
+    TX_EN           <= rx_det_stable;
     
     tx_channels_out <= rxpt_tx_channels_out;
+    
+    rx_det_DEBOUNCE_inst : entity work.DEBOUNCE
+        generic map (
+            CYCLE_COUNT => 100
+        )
+        port map (
+            CLK => g_clk,
+            
+            I   => RX_DET(RX_SEL),
+            O   => rx_det_stable
+        );
+        
+    tx_det_DEBOUNCE_inst : entity work.DEBOUNCE
+        generic map (
+            CYCLE_COUNT => 100
+        )
+        port map (
+            CLK => g_clk,
+            
+            I   => TX_DET,
+            O   => tx_det_stable
+        );
     
     diff_ibuf_gen : for i in 0 to 7 generate
         
@@ -296,6 +209,39 @@ begin
             );
         
     end generate;
+    
+    
+    ----------------------------
+    --- HDMI DDC passthrough ---
+    ----------------------------
+    
+    scl_BIDIR_REPEAT_BUFFER_inst : entity work.BIDIR_REPEAT_BUFFER
+        generic map (
+            PULL    => "UP",
+            FLOAT   => true
+        )
+        port map (
+            CLK => g_clk,
+            
+            P0_IN   => RX_SCL(RX_SEL),
+            P0_OUT  => RX_SCL(RX_SEL),
+            P1_IN   => TX_SCL,
+            P1_OUT  => TX_SCL
+        );
+    
+    sda_BIDIR_REPEAT_BUFFER_inst : entity work.BIDIR_REPEAT_BUFFER
+        generic map (
+            PULL => "UP",
+            FLOAT   => true
+        )
+        port map (
+            CLK => g_clk,
+            
+            P0_IN   => RX_SDA(RX_SEL),
+            P0_OUT  => RX_SDA(RX_SEL),
+            P1_IN   => TX_SDA,
+            P1_OUT  => TX_SDA
+        );
     
     
     ----------------------------------
@@ -331,7 +277,7 @@ begin
     rx_pix_clk          <= rxclk_clk_out2;
     rx_pix_clk_x2       <= rxclk_clk_out1;
     rx_pix_clk_x10      <= rxclk_ioclk_out;
-    rx_rst              <= not RX_DET(RX_SEL);
+    rx_rst              <= not tx_det_stable;
     rx_clk_locked       <= rxclk_ioclk_locked;
     rx_serdesstrobe     <= rxclk_serdesstrobe;
     
@@ -355,116 +301,6 @@ begin
             RGB             => rx_rgb,
             AUX_DATA        => rx_aux_data,
             AUX_DATA_VALID  => rx_aux_data_valid
-        );
-    
-    
-    ---------------------------
-    --- LED color extractor ---
-    ---------------------------
-    
-    ledex_clk   <= rx_pix_clk;
-    ledex_rst   <= rx_rst;
-    
-    ledex_cfg_addr  <= stdulv(microblaze_gpo3(11 downto 8));
-    ledex_cfg_wr_en <= stdul(microblaze_gpo3(12));
-    ledex_cfg_data  <= stdulv(microblaze_gpo3(7 downto 0));
-    
-    ledex_frame_vsync   <= rx_vsync;
-    ledex_frame_hsync   <= rx_hsync;
-    
-    ledex_frame_rgb <= rx_rgb;
-    
-    LED_COLOR_EXTRACTOR_inst : entity work.LED_COLOR_EXTRACTOR
-        port map (
-            CLK => ledex_clk,
-            RST => ledex_rst,
-            
-            CFG_ADDR    => ledex_cfg_addr,
-            CFG_WR_EN   => ledex_cfg_wr_en,
-            CFG_DATA    => ledex_cfg_data,
-            
-            FRAME_VSYNC => ledex_frame_vsync,
-            FRAME_HSYNC => ledex_frame_hsync,
-            
-            FRAME_RGB   => ledex_frame_rgb,
-            
-            LED_VSYNC   => ledex_led_vsync,
-            LED_VALID   => ledex_led_valid,
-            LED_NUM     => ledex_led_num,
-            LED_RGB     => ledex_led_rgb
-        );
-    
-    
-    -------------------
-    --- LED control ---
-    -------------------
-    
-    ledctrl_clk <= rx_pix_clk;
-    ledctrl_rst <= rx_rst;
-    
-    ledctrl_mode    <= stdulv(microblaze_gpo3(14 downto 13));
-    
-    ledctrl_led_vsync       <= ledcorr_led_out_vsync;
-    ledctrl_led_rgb         <= ledcorr_led_out_rgb;
-    ledctrl_led_rgb_wr_en   <= ledcorr_led_out_valid;
-    
-    LED_CONTROL_inst : entity work.LED_CONTROL
-        generic map (
-            CLK_IN_PERIOD           => G_CLK_PERIOD,
-            WS2801_LEDS_CLK_PERIOD  => WS2801_CLK_PERIOD
-        )
-        port map (
-            CLK => ledctrl_clk,
-            RST => ledctrl_rst,
-            
-            MODE    => ledctrl_mode,
-            
-            LED_VSYNC       => ledctrl_led_vsync,
-            LED_RGB         => ledctrl_led_rgb,
-            LED_RGB_WR_EN   => ledctrl_led_rgb_wr_en,
-            
-            LEDS_CLK    => ledctrl_leds_clk,
-            LEDS_DATA   => ledctrl_leds_data
-        );
-    
-    
-    ----------------------
-    --- LED correction ---
-    ----------------------
-    
-    ledcorr_clk <= rx_pix_clk;
-    ledcorr_rst <= rx_rst;
-    
-    ledcorr_cfg_addr    <= stdulv(microblaze_gpo3(25 downto 24));
-    ledcorr_cfg_wr_en   <= stdul(microblaze_gpo3(26));
-    ledcorr_cfg_data    <= stdulv(microblaze_gpo3(23 downto 16));
-    
-    ledcorr_led_in_vsync    <= ledex_led_vsync;
-    ledcorr_led_in_num      <= ledex_led_num;
-    ledcorr_led_in_wr_en    <= ledex_led_valid;
-    ledcorr_led_in_rgb      <= ledex_led_rgb;
-    
-    LED_CORRECTION_inst : entity work.LED_CORRECTION
-        generic map (
-            MAX_LED_COUNT   => MAX_LED_COUNT,
-            MAX_BUFFER_SIZE => MAX_LED_BUFFER_SIZE
-        )
-        port map (
-            CLK => ledcorr_clk,
-            RST => ledcorr_rst,
-            
-            CFG_ADDR    => ledcorr_cfg_addr,
-            CFG_WR_EN   => ledcorr_cfg_wr_en,
-            CFG_DATA    => ledcorr_cfg_data,
-            
-            LED_IN_VSYNC    => ledcorr_led_in_vsync,
-            LED_IN_NUM      => ledcorr_led_in_num,
-            LED_IN_WR_EN    => ledcorr_led_in_wr_en,
-            LED_IN_RGB      => ledcorr_led_in_rgb,
-            
-            LED_OUT_VSYNC   => ledcorr_led_out_vsync,
-            LED_OUT_VALID   => ledcorr_led_out_valid,
-            LED_OUT_RGB     => ledcorr_led_out_rgb
         );
     
     
