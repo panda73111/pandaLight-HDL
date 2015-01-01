631dfb4a473eefca49518328286e0d4970f6a9bd
Sebastian Hüther <sebastian.huether@gmx.de>
2015-01-01 21:50:28 +0100

    Corrected/Expanded LED_COLOR_EXTRACTOR_tb; The results of the smaller pattern look... interesting

--- LED_COLOR_EXTRACTOR/LED_COLOR_EXTRACTOR_tb.vhd
+++ LED_COLOR_EXTRACTOR/LED_COLOR_EXTRACTOR_tb.vhd
@@ -9,8 +9,6 @@
 -- 
 -- VHDL Test Bench Created by ISE for module: LED_COLOR_EXTRACTOR
 -- 
--- Revision: 0
--- Revision 0.01 - File Created
 -- Additional Comments:
 --
 --------------------------------------------------------------------------------
@@ -21,62 +19,82 @@ library UNISIM;
 use UNISIM.VComponents.all;
 use work.help_funcs.all;
 use work.txt_util.all;
+use work.video_profiles.all;
 
 ENTITY LED_COLOR_EXTRACTOR_tb IS
+    generic (
+        R_BITS  : natural range 1 to 12 := 8;
+        G_BITS  : natural range 1 to 12 := 8;
+        B_BITS  : natural range 1 to 12 := 8
+    );
 END LED_COLOR_EXTRACTOR_tb;
 
 ARCHITECTURE behavior OF LED_COLOR_EXTRACTOR_tb IS 
     
-    constant R_BITS          : natural range 1 to 12 := 8;
-    constant G_BITS          : natural range 1 to 12 := 8;
-    constant B_BITS          : natural range 1 to 12 := 8;
-    
     -- Inputs
-    signal clk, rst : std_ulogic := '0';
+    signal CLK  : std_ulogic := '0';
+    signal RST  : std_ulogic := '0';
     
-    signal cfg_addr     : std_ulogic_vector(3 downto 0) := "0000";
-    signal cfg_wr_en    : std_ulogic := '0';
-    signal cfg_data     : std_ulogic_vector(7 downto 0) := x"00";
+    signal CFG_ADDR     : std_ulogic_vector(3 downto 0) := "0000";
+    signal CFG_WR_EN    : std_ulogic := '0';
+    signal CFG_DATA     : std_ulogic_vector(7 downto 0) := x"00";
     
-    signal frame_vsync, frame_hsync     : std_ulogic := '0';
-    
-    signal frame_r  : std_ulogic_vector(R_BITS-1 downto 0) := (others => '0');
-    signal frame_g  : std_ulogic_vector(G_BITS-1 downto 0) := (others => '0');
-    signal frame_b  : std_ulogic_vector(B_BITS-1 downto 0) := (others => '0');
+    signal FRAME_VSYNC      : std_ulogic := '0';
+    signal FRAME_RGB_WR_EN  : std_ulogic := '0';
+    signal FRAME_RGB        : std_ulogic_vector(R_BITS+G_BITS+B_BITS-1 downto 0) := (others => '0');
 
     --Outputs
-    signal led_vsync    : std_ulogic := '0';
-    signal led_valid    : std_ulogic := '0';
-    signal led_num      : std_ulogic_vector(7 downto 0) := (others => '0');
-    signal led_r        : std_ulogic_vector(R_BITS-1 downto 0) := (others => '0');
-    signal led_g        : std_ulogic_vector(G_BITS-1 downto 0) := (others => '0');
-    signal led_b        : std_ulogic_vector(B_BITS-1 downto 0) := (others => '0');
+    signal LED_VSYNC    : std_ulogic := '0';
+    signal LED_VALID    : std_ulogic := '0';
+    signal LED_NUM      : std_ulogic_vector(7 downto 0) := (others => '0');
+    signal LED_RGB      : std_ulogic_vector(R_BITS+G_BITS+B_BITS-1 downto 0) := (others => '0');
+
 
     -- Clock period definitions
-    constant clk_period : time := 10 ns; -- 100 MHz
+    constant G_CLK_PERIOD       : time := 10 ns; -- 100 MHz
+    constant G_CLK_PERIOD_REAL  : real := real(G_CLK_PERIOD / 1 ps) / real(1 ns / 1 ps);
+    
+    signal g_clk            : std_ulogic := '0';
+    signal rst_extr         : std_ulogic := '0';
+    signal pix_clk          : std_ulogic := '0';
+    signal pix_clk_locked   : std_ulogic := '0';
     
     signal frame_width, frame_height    : std_ulogic_vector(15 downto 0) := x"0000";
     
+    signal vp       : video_profile_type;
+    signal profile  : std_ulogic_vector(log2(VIDEO_PROFILE_COUNT)-1 downto 0) := (others => '0');
+    
 BEGIN
     
-    test_frame_gen_inst : entity work.test_camera
+    CLK         <= pix_clk;
+    rst_extr    <= RST or not pix_clk_locked;
+    
+    vp  <= VIDEO_PROFILES(nat(profile));
+    
+    frame_width     <= stdulv( vp.width, 16);
+    frame_height    <= stdulv(vp.height, 16);
+    
+    TEST_FRAME_GEN_inst : entity work.TEST_FRAME_GEN
     generic map (
+        CLK_IN_PERIOD   => G_CLK_PERIOD_REAL,
         FRAME_STEP      => 0,
         R_BITS          => R_BITS,
         G_BITS          => G_BITS,
         B_BITS          => B_BITS
     )
     port map (
-        CLK => clk,
-        RST => rst,
+        CLK_IN  => g_clk,
+        RST     => RST,
         
-        WIDTH   => frame_width,
-        HEIGHT  => frame_height,
-        HSYNC   => frame_hsync,
-        VSYNC   => frame_vsync,
-        R       => frame_r,
-        G       => frame_g,
-        B       => frame_b
+        PROFILE => profile,
+        
+        CLK_OUT         => pix_clk,
+        CLK_OUT_LOCKED  => pix_clk_locked,
+        
+        POSITIVE_VSYNC  => frame_vsync,
+        
+        RGB_ENABLE  => FRAME_RGB_WR_EN,
+        RGB         => FRAME_RGB
     );
     
     led_ppm_visualizer_inst : entity work.led_ppm_visualizer
@@ -89,19 +107,21 @@ BEGIN
         B_BITS          => B_BITS
     )
     port map (
-        CLK => clk,
-        RST => rst,
+        CLK => CLK,
+        RST => rst_extr,
         
-        CFG_ADDR    => cfg_addr,
-        CFG_WR_EN   => cfg_wr_en,
-        CFG_DATA    => cfg_data,
+        CFG_ADDR    => CFG_ADDR,
+        CFG_WR_EN   => CFG_WR_EN,
+        CFG_DATA    => CFG_DATA,
         
-        LED_VSYNC   => led_vsync,
-        LED_VALID   => led_valid,
-        LED_NUM     => led_num,
-        LED_R       => led_r,
-        LED_G       => led_g,
-        LED_B       => led_b
+        FRAME_VSYNC     => FRAME_VSYNC,
+        FRAME_RGB_WR_EN => FRAME_RGB_WR_EN,
+        FRAME_RGB       => FRAME_RGB,
+        
+        LED_VSYNC   => LED_VSYNC,
+        LED_VALID   => LED_VALID,
+        LED_NUM     => LED_NUM,
+        LED_RGB     => LED_RGB
     );
     
     LED_COLOR_EXTRACTOR_inst : entity work.LED_COLOR_EXTRACTOR
@@ -111,30 +131,25 @@ BEGIN
         B_BITS          => B_BITS
     )
     port map (
-        CLK => clk,
-        RST => rst,
+        CLK => CLK,
+        RST => rst_extr,
         
-        CFG_ADDR    => cfg_addr,
-        CFG_WR_EN   => cfg_wr_en,
-        CFG_DATA    => cfg_data,
+        CFG_ADDR    => CFG_ADDR,
+        CFG_WR_EN   => CFG_WR_EN,
+        CFG_DATA    => CFG_DATA,
         
-        FRAME_VSYNC     => frame_vsync,
-        FRAME_HSYNC     => frame_hsync,
+        FRAME_VSYNC     => FRAME_VSYNC,
+        FRAME_RGB_WR_EN => FRAME_RGB_WR_EN,
+        FRAME_RGB       => FRAME_RGB,
         
-        FRAME_R => frame_r,
-        FRAME_G => frame_g,
-        FRAME_B => frame_b,
-        
-        LED_VSYNC   => led_vsync,
-        LED_VALID   => led_valid,
-        LED_NUM     => led_num,
-        LED_R       => led_r,
-        LED_G       => led_g,
-        LED_B       => led_b
+        LED_VSYNC   => LED_VSYNC,
+        LED_VALID   => LED_VALID,
+        LED_NUM     => LED_NUM,
+        LED_RGB     => LED_RGB
     );
 
     -- clock generation
-    clk <= not clk after clk_period / 2;
+    g_clk   <= not g_clk after G_CLK_PERIOD / 2;
     
     
     -- Stimulus process
@@ -149,25 +164,27 @@ BEGIN
         procedure write_config (cfg : in cfg_type) is
         begin
             cfg_wr_en   <= '1';
-            for i in 0 to 15 loop
+            for i in cfg_type'range loop
                 cfg_addr    <= stdulv(i, 4);
                 cfg_data    <= cfg(i);
-                wait until rising_edge(clk);
+                wait until rising_edge(CLK);
             end loop;
             cfg_wr_en   <= '0';
-            wait until rising_edge(clk);
+            wait until rising_edge(CLK);
         end procedure;
         
     begin
         
-        frame_width     <= stdulv(1280, 16);
-        frame_height    <= stdulv(720,  16);
-        
         -- hold reset state for 100 ns.
         rst <= '1';
-        wait for 100 ns;
+        wait for 200 ns;
         rst <= '0';
-        wait until rising_edge(clk);
+        wait until rising_edge(pix_clk_locked);
+        
+        profile <= stdulv(VIDEO_PROFILE_1280_720p_60, profile'length);
+        wait until rising_edge(pix_clk_locked);
+        
+        wait until rising_edge(FRAME_VSYNC);
         
         -- Test 1: Standard 50 LED configuration, no overlap, no edges
         
@@ -192,8 +209,7 @@ BEGIN
         write_config(cfg);
         
         for i in 1 to 5 loop
-            wait until rising_edge(clk) and frame_vsync='1';
-            wait until rising_edge(clk) and frame_vsync='0';
+            wait until rising_edge(FRAME_VSYNC);
         end loop;
         
         -- Test 1 finished
--- LED_COLOR_EXTRACTOR/led_ppm_visualizer.vhd
+++ LED_COLOR_EXTRACTOR/led_ppm_visualizer.vhd
@@ -43,12 +43,14 @@ entity led_ppm_visualizer is
         CFG_WR_EN   : in std_ulogic;
         CFG_DATA    : in std_ulogic_vector(7 downto 0);
         
+        FRAME_VSYNC     : in std_ulogic;
+        FRAME_RGB_WR_EN : in std_ulogic;
+        FRAME_RGB       : in std_ulogic_vector(R_BITS+G_BITS+B_BITS-1 downto 0);
+        
         LED_VSYNC   : in std_ulogic;
         LED_VALID   : in std_ulogic;
         LED_NUM     : in std_ulogic_vector(7 downto 0);
-        LED_R       : in std_ulogic_vector(R_BITS-1 downto 0);
-        LED_G       : in std_ulogic_vector(G_BITS-1 downto 0);
-        LED_B       : in std_ulogic_vector(B_BITS-1 downto 0)
+        LED_RGB     : in std_ulogic_vector(R_BITS+G_BITS+B_BITS-1 downto 0)
     );
 end led_ppm_visualizer;
 
@@ -73,6 +75,10 @@ architecture rtl of led_ppm_visualizer is
     signal frame_width      : std_ulogic_vector(15 downto 0) := x"0000";
     signal frame_height     : std_ulogic_vector(15 downto 0) := x"0000";
     
+    signal configured           : boolean := false;
+    signal led_imgs_finished    : boolean := false;
+    signal src_imgs_finished    : boolean := false;
+    
     type char_file is file of character;
     type data_type is (BYTE, HWORD, WORD);
     
@@ -84,12 +90,12 @@ architecture rtl of led_ppm_visualizer is
         array(0 to MAX_WIDTH-1) of
         frame_col_type;
     
-    function to_char (v : std_ulogic_vector) return character is
+    function to_char(v : std_ulogic_vector) return character is
     begin
         return character'val(int(uns(v)));
     end to_char;
 
-    procedure file_write (file f : char_file; v : std_ulogic_vector) is
+    procedure file_write(file f : char_file; v : std_ulogic_vector) is
         variable t : std_ulogic_vector(v'length-1 downto 0) := v;
         variable i : natural := 0;
     begin
@@ -99,27 +105,64 @@ architecture rtl of led_ppm_visualizer is
         end loop;
     end file_write;
     
-    procedure file_write (file f : char_file; c : character) is
+    procedure file_write(file f : char_file; constant c : character) is
     begin
         write(f, c);
     end procedure;
     
-    procedure file_write (file f : char_file; s : string) is
+    procedure file_write(file f : char_file; constant s : string) is
         variable v : std_ulogic_vector(s'length*8-1 downto 0);
     begin
         for i in 1 to s'length loop
             write(f, s(i));
         end loop;
-    end file_write;
+    end procedure;
 
-    procedure file_write (file f : char_file; i : integer; t : data_type) is
+    procedure file_write(file f : char_file; i : integer; constant t : data_type) is
     begin
         case t is
             when BYTE   => file_write(f, stdulv(uns(i, 8)));
             when HWORD  => file_write(f, stdulv(uns(i, 16)));
             when WORD   => file_write(f, stdulv(uns(i, 32)));
         end case;
-    end file_write;
+    end procedure;
+    
+    procedure img_file_write(constant filename : string; frame_buf : in frame_buf_type) is
+        file img_file   : char_file;
+        variable pixel  : std_ulogic_vector(RGB_BITS-1 downto 0);
+        variable r      : std_ulogic_vector(R_BITS-1 downto 0);
+        variable g      : std_ulogic_vector(G_BITS-1 downto 0);
+        variable b      : std_ulogic_vector(B_BITS-1 downto 0);
+    begin
+        report("opening file " & filename);
+        file_open(img_file, filename, write_mode);
+        
+        -- write the file header
+        file_write(img_file, "P6");                   -- magic number
+        file_write(img_file, WHITESPACE_CHAR);
+        file_write(img_file, str(int(frame_width)));  -- width
+        file_write(img_file, WHITESPACE_CHAR);
+        file_write(img_file, str(int(frame_height))); -- height
+        file_write(img_file, WHITESPACE_CHAR);
+        file_write(img_file, str(MAXVAL));            -- Maxval
+        file_write(img_file, WHITESPACE_CHAR);
+        
+        for y in 0 to nat(frame_height)-1 loop
+            for x in 0 to nat(frame_width)-1 loop
+                pixel   := frame_buf(x)(y);
+                r       := pixel(pixel'left downto pixel'left-R_BITS+1);
+                g       := pixel(pixel'left-R_BITS downto B_BITS);
+                b       := pixel(B_BITS-1 downto 0);
+                file_write(img_file, r);
+                file_write(img_file, g);
+                file_write(img_file, b);
+            end loop;
+            --file_write(img_file, character'val(13)); -- newline
+        end loop;
+        
+        report("closing file " & filename);
+        file_close(img_file);
+    end procedure;
 begin
     
     cfg_proc : process(RST, CLK)
@@ -139,54 +182,116 @@ begin
             ver_led_offs    <= x"00";
             frame_width     <= x"0000";
             frame_height    <= x"0000";
+            configured      <= false;
         elsif rising_edge(CLK) then
             if CFG_WR_EN='1' then
                 case CFG_ADDR is
-                    when "0000" => hor_led_cnt                  <= CFG_DATA;
-                    when "0001" => hor_led_width                <= CFG_DATA;
-                    when "0010" => hor_led_height               <= CFG_DATA;
-                    when "0011" => hor_led_step                 <= CFG_DATA;
-                    when "0100" => hor_led_pad                  <= CFG_DATA;
-                    when "0101" => hor_led_offs                 <= CFG_DATA;
-                    when "0110" => ver_led_cnt                  <= CFG_DATA;
-                    when "0111" => ver_led_width                <= CFG_DATA;
-                    when "1000" => ver_led_height               <= CFG_DATA;
-                    when "1001" => ver_led_step                 <= CFG_DATA;
-                    when "1010" => ver_led_pad                  <= CFG_DATA;
-                    when "1011" => ver_led_offs                 <= CFG_DATA;
-                    when "1100" => frame_width(15 downto 8)     <= CFG_DATA;
-                    when "1101" => frame_width(7 downto 0)      <= CFG_DATA;
-                    when "1110" => frame_height(15 downto 8)    <= CFG_DATA;
-                    when "1111" => frame_height(7 downto 0)     <= CFG_DATA;
-                    when others => null;
+                    when "0000" =>  hor_led_cnt                  <= CFG_DATA;
+                    when "0001" =>  hor_led_width                <= CFG_DATA;
+                    when "0010" =>  hor_led_height               <= CFG_DATA;
+                    when "0011" =>  hor_led_step                 <= CFG_DATA;
+                    when "0100" =>  hor_led_pad                  <= CFG_DATA;
+                    when "0101" =>  hor_led_offs                 <= CFG_DATA;
+                    when "0110" =>  ver_led_cnt                  <= CFG_DATA;
+                    when "0111" =>  ver_led_width                <= CFG_DATA;
+                    when "1000" =>  ver_led_height               <= CFG_DATA;
+                    when "1001" =>  ver_led_step                 <= CFG_DATA;
+                    when "1010" =>  ver_led_pad                  <= CFG_DATA;
+                    when "1011" =>  ver_led_offs                 <= CFG_DATA;
+                    when "1100" =>  frame_width(15 downto 8)     <= CFG_DATA;
+                    when "1101" =>  frame_width(7 downto 0)      <= CFG_DATA;
+                    when "1110" =>  frame_height(15 downto 8)    <= CFG_DATA;
+                    when others =>  frame_height(7 downto 0)     <= CFG_DATA;
+                                    configured  <= true;
                 end case;
             end if;
         end if;
     end process;
     
-    write_file_proc : process
+    write_src_file_proc : process
+        variable filename   : string(1 to FILENAME_BASE'length+10);
+        variable file_index : integer := 0;
+        variable frame_buf  : frame_buf_type;
+        variable x          : natural range 0 to MAX_WIDTH;
+        variable y          : natural range 0 to MAX_HEIGHT;
+        variable side_led_index : natural range 0 to 255;
+    begin
+        wait until configured;
+        frame_loop: while file_index/=frames_to_save loop
+            filename    := FILENAME_BASE & integer'image(file_index) & "_src.ppm";
+            frame_buf   := (others => (others => (others => '0')));
+            
+            wait until falling_edge(FRAME_VSYNC);
+            
+            -- start of frame
+            x   := 0;
+            y   := 0;
+            
+            while FRAME_VSYNC='0' loop
+                wait until rising_edge(CLK) or RST='1';
+                
+                if RST='1' then
+                    next frame_loop;
+                end if;
+                
+                if FRAME_RGB_WR_EN='0' then
+                    next;
+                end if;
+                
+                while FRAME_RGB_WR_EN='1' loop
+                    frame_buf(x)(y) := FRAME_RGB;
+                    x               := x+1;
+                    wait until rising_edge(CLK) or RST='1';
+                    
+                    if RST='1' then
+                        next frame_loop;
+                    end if;
+                end loop;
+                
+                -- end of line
+                x   := 0;
+                y   := y+1;
+            end loop;
+            
+            img_file_write(filename, frame_buf);
+            
+            file_index  := file_index+1;
+        end loop;
+        
+        src_imgs_finished   <= true;
+        
+        assert not stop_sim or not led_imgs_finished
+            report "NONE. Saved " & integer'image(frames_to_save) & " frame(s)."
+            severity FAILURE;
+        
+        wait;
+    end process;
+    
+    write_led_file_proc : process
         file img_file       : char_file;
         variable filename   : string(1 to FILENAME_BASE'length+6);
         variable file_index : integer := 0;
         variable frame_buf  : frame_buf_type;
-        variable pixel      : std_ulogic_vector(RGB_BITS-1 downto 0);
-        variable r          : std_ulogic_vector(R_BITS-1 downto 0);
-        variable g          : std_ulogic_vector(G_BITS-1 downto 0);
-        variable b          : std_ulogic_vector(B_BITS-1 downto 0);
         variable led_start_x    : natural range 0 to MAX_WIDTH-1;
         variable led_end_x      : natural range 0 to MAX_WIDTH-1;
         variable led_start_y    : natural range 0 to MAX_HEIGHT-1;
         variable led_end_y      : natural range 0 to MAX_HEIGHT-1;
         variable side_led_index : natural range 0 to 255;
     begin
-        while file_index/=frames_to_save loop
+        wait until configured;
+        frame_loop: while file_index/=frames_to_save loop
             filename    := FILENAME_BASE & integer'image(file_index) & ".ppm";
             frame_buf   := (others => (others => (others => '0')));
             
-            wait until LED_VSYNC='1';
+            wait until falling_edge(LED_VSYNC);
             
-            while LED_VSYNC='1' loop
-                wait until rising_edge(CLK);
+            while LED_VSYNC='0' loop
+                wait until rising_edge(CLK) or RST='1';
+                
+                if RST='1' then
+                    next frame_loop;
+                end if;
+                
                 if LED_VALID='1' then
                     if LED_NUM<hor_led_cnt then
                         
@@ -229,46 +334,21 @@ begin
                     -- draw the LED
                     for y in led_start_y to led_end_y loop
                         for x in led_start_x to led_end_x loop
-                            frame_buf(x)(y) := LED_R & LED_G & LED_B;
+                            frame_buf(x)(y) := LED_RGB;
                         end loop;
                     end loop;
                     
                 end if;
             end loop;
             
-            report("opening file " & filename);
-            file_open(img_file, filename, write_mode);
-            
-            -- write the file header
-            file_write(img_file, "P6");                   -- magic number
-            file_write(img_file, WHITESPACE_CHAR);
-            file_write(img_file, str(int(frame_width)));  -- width
-            file_write(img_file, WHITESPACE_CHAR);
-            file_write(img_file, str(int(frame_height))); -- height
-            file_write(img_file, WHITESPACE_CHAR);
-            file_write(img_file, str(MAXVAL));            -- Maxval
-            file_write(img_file, WHITESPACE_CHAR);
-            
-            for y in 0 to int(FRAME_HEIGHT-1) loop
-                for x in 0 to int(FRAME_WIDTH-1) loop
-                    pixel   := frame_buf(x)(y);
-                    r       := pixel(pixel'left downto pixel'left-R_BITS+1);
-                    g       := pixel(pixel'left-R_BITS downto B_BITS);
-                    b       := pixel(B_BITS-1 downto 0);
-                    file_write(img_file, r);
-                    file_write(img_file, g);
-                    file_write(img_file, b);
-                end loop;
-                --file_write(img_file, character'val(13)); -- newline
-            end loop;
-            
-            report("closing file " & filename);
-            file_close(img_file);
+            img_file_write(filename, frame_buf);
             
             file_index  := file_index+1;
         end loop;
         
-        assert not stop_sim
+        led_imgs_finished   <= true;
+        
+        assert not stop_sim or not src_imgs_finished
             report "NONE. Saved " & integer'image(frames_to_save) & " frame(s)."
             severity FAILURE;
         
